4b5c3208a1e421ab7a1c0993c8cc30585aa1bd1c
marcjabbourduke <marc.jabbour@me.com>
2021-03-22 14:06:59 -0400

    Pressure View buttons render divs

--- smartsock_react/src/views/pressure_insole.js
+++ smartsock_react/src/views/pressure_insole.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState }  from 'react'
 
 import Helmet from 'react-helmet'
 
 import ViewRec from '../components/view-rec'
+import CreateRec from '../components/create-rec'
+
 import projectStyles from '../style.module.css'
 import styles from './pressure_insole.module.css'
 
 const PressureInsole = () => {
+  const [viewRec, setViewRec] = useState(false)
+  const [createRec, setCreateRec] = useState(false)
+
+  const showViewRec = () => {setViewRec(true); setCreateRec(false)}
+  const showCreateRec = () => {setViewRec(false); setCreateRec(true)}
+
   return (
     <div className={styles.container}>
       <Helmet>
@@ -19,12 +27,13 @@ const PressureInsole = () => {
       </svg>
       <span className={styles.text}>Pressure Insole</span>
       <div className={styles.container1}>
-        <button className={` ${projectStyles.thqButton} ${styles.button} `}>View Recordings</button>
-        <button className={` ${projectStyles.thqButton} ${styles.button1} `}>
+        <button className={` ${projectStyles.thqButton} ${styles.button} `} onClick={showViewRec}>View Recordings</button>
+        <button className={` ${projectStyles.thqButton} ${styles.button1} `} onClick={showCreateRec}>
           Create New Recording
         </button>
       </div>
-      <ViewRec></ViewRec>
+        {viewRec ? <ViewRec /> : null}
+        {createRec ? <CreateRec /> : null} 
     </div>
   )
 }
